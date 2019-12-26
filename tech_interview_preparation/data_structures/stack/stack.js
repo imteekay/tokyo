f5de3711c437969cc1b2de95a6326c75f5aa776c
TK <leandrotk100@gmail.com>
2019-12-26 01:33:22 -0300

    Stack

---
+++ tech_interview_preparation/data_structures/stack/stack.js
@@ -0,0 +1,45 @@
+// push
+const push = (stack, item) => [item, ...stack];
+
+// pop
+const pop = (stack) => stack.slice(1);
+
+// peek
+const peek = (stack) => stack[0];
+
+// isEmpty
+const isEmpty = (stack) => stack[0] === undefined;
+
+// size
+const size = (stack) => stack.length;
+
+// testing
+let stack = [];
+
+console.log(isEmpty(stack));
+
+stack = push(stack, 1);
+console.log(stack);
+stack = push(stack, 2);
+console.log(stack);
+stack = push(stack, 3);
+console.log(stack);
+stack = push(stack, 4);
+console.log(stack);
+stack = push(stack, 5);
+console.log(stack);
+
+console.log(peek(stack));
+console.log(size(stack));
+console.log(isEmpty(stack));
+
+stack = pop(stack);
+stack = pop(stack);
+stack = pop(stack);
+stack = pop(stack);
+
+console.log(isEmpty(stack));
+console.log(peek(stack));
+stack = pop(stack);
+console.log(isEmpty(stack));
+console.log(size(stack));
