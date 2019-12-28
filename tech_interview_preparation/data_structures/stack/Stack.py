1f08b92e39007e362fa156af7fd89469d9e7f253
TK <leandrotk100@gmail.com>
2019-12-28 09:41:05 -0300

    Stack: big o

--- tech_interview_preparation/data_structures/stack/Stack.py
+++ tech_interview_preparation/data_structures/stack/Stack.py
@@ -1,3 +1,12 @@
+# Stack Big O complexity
+
+# Push: O(1) - Constant Time
+# Pop (remove): O(1) - Constant Time
+# Peek (top): O(1) - Constant Time
+# Is Empty: O(1) - Constant Time
+# Size: O(1) - Constant Time
+
+
 class Stack:
     def __init__(self):
         self.items = []
