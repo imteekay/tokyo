daebf69164f382ebc4ce5fec850a854ded5077a9
TK <leandrotk100@gmail.com>
2019-12-28 09:41:21 -0300

    Queue: big o

--- tech_interview_preparation/data_structures/queue/Queue.py
+++ tech_interview_preparation/data_structures/queue/Queue.py
@@ -1,3 +1,13 @@
+# Queue Big O complexity
+
+# Enqueue (push): O(1) - Constant Time
+# Dequeue (remove): O(1) - Constant Time
+# Front: O(1) - Constant Time
+# Back: O(1) - Constant Time
+# Is Empty: O(1) - Constant Time
+# Size: O(1) - Constant Time
+
+
 class Queue:
     def __init__(self):
         self.items = []
