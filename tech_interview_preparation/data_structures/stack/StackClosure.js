ad556ded7776f9f1dd90a7937784d7505a6a3b44
TK <leandrotk100@gmail.com>
2019-12-29 14:17:23 -0300

    Fix stack closure

--- tech_interview_preparation/data_structures/stack/StackClosure.js
+++ tech_interview_preparation/data_structures/stack/StackClosure.js
@@ -1,18 +1,20 @@
 const createStack = () => {
-  const items = [];
+  let items = [];
 
-  const push = (item) => [item, ...items];
-  const pop = () => items.slice(1);
+  const push = (item) => items = [item, ...items];
+  const pop = () => items = items.slice(1);
   const top = () => items[0];
-  const isEmpty = () => !items.length;
+  const isEmpty = () => size() === 0;
   const size = () => items.length;
+  const getItems = () => items;
 
   return {
     push,
     pop,
     top,
     isEmpty,
-    size
+    size,
+    getItems
   };
 };
 
@@ -22,27 +24,32 @@ const stack = createStack();
 console.log(stack.isEmpty());
 
 stack.push(1);
-console.log(stack.items);
+console.log(stack.getItems());
 stack.push(2);
-console.log(stack.items);
+console.log(stack.getItems());
 stack.push(3);
-console.log(stack.items);
+console.log(stack.getItems());
 stack.push(4);
-console.log(stack.items);
+console.log(stack.getItems());
 stack.push(5);
-console.log(stack.items);
+console.log(stack.getItems());
 
 console.log(stack.top());
 console.log(stack.size());
 console.log(stack.isEmpty());
 
 stack.pop();
+console.log(stack.getItems());
 stack.pop();
+console.log(stack.getItems());
 stack.pop();
+console.log(stack.getItems());
 stack.pop();
+console.log(stack.getItems());
 
 console.log(stack.isEmpty());
 console.log(stack.top());
 stack.pop();
+console.log(stack.getItems());
 console.log(stack.isEmpty());
 console.log(stack.size());
