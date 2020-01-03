7d8382e5241880db76097b3b15405e147b1199fc
TK <leandrotk100@gmail.com>
2020-01-03 15:02:34 -0300

    implement search method

--- tech_interview_preparation/data_structures/linked_list/LinkedList.py
+++ tech_interview_preparation/data_structures/linked_list/LinkedList.py
@@ -42,8 +42,19 @@ class LinkedList:
 
             current_node = current_node.next
 
+    def search(self, value):
+        found = False
+        current_node = self.head
+
+        while not found and current_node is not None:
+            found = current_node.value == value
+            current_node = current_node.next
+
+        return found
+
 
 def print_all(linked_list):
+    print('All values:', end=' ')
     current_node = linked_list.head
 
     while current_node is not None:
@@ -51,6 +62,11 @@ def print_all(linked_list):
         current_node = current_node.next
 
     print()
+
+
+def print_found(linked_list, value):
+    found = linked_list.search(value)
+    print('For value:', value, '-->', 'Found:', found, )
 
 
 linked_list = LinkedList()
@@ -68,6 +84,10 @@ print_all(linked_list)
 linked_list.append(2)
 linked_list.append(3)
 print_all(linked_list)
+
+print_found(linked_list, 1)
+print_found(linked_list, 2)
+print_found(linked_list, 3)
 
 linked_list.remove(1)
 print_all(linked_list)
