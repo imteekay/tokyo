5128c99eb0c288eee787d1e499531db2def2b822
TK <leandrotk100@gmail.com>
2020-01-04 23:53:54 -0300

    Implement singly linked list with tail

--- tech_interview_preparation/data_structures/linked_list/SinglyLinkedListWithTail.py
+++ tech_interview_preparation/data_structures/linked_list/SinglyLinkedListWithTail.py
@@ -4,26 +4,25 @@ class Node:
         self.next = None
 
 
-class LinkedList:
+class SinglyLinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
 
     def append(self, value):
         if self.is_empty():
             self.head = Node(value)
+            self.tail = self.head
             return
 
-        current_node = self.head
-
-        while current_node.next is not None:
-            current_node = current_node.next
-
-        current_node.next = Node(value)
+        old_tail = self.tail
+        self.tail = Node(value)
+        old_tail.next = self.tail
 
     def prepend(self, value):
-        previous_head = self.head
+        old_head = self.head
         self.head = Node(value)
-        self.head.next = previous_head
+        self.head.next = old_head
 
     def remove(self, value):
         if self.is_empty():
@@ -36,21 +35,14 @@ class LinkedList:
         current_node = self.head
 
         while current_node.next is not None:
-            if current_node.value == value:
+            if current_node.next.value == value:
                 current_node.next = current_node.next.next
                 return
 
             current_node = current_node.next
 
     def search(self, value):
-        found = False
-        current_node = self.head
-
-        while not found and current_node is not None:
-            found = current_node.value == value
-            current_node = current_node.next
-
-        return found
+        pass
 
     def is_empty(self):
         return self.head is None
@@ -87,9 +79,8 @@ def print_size(linked_list):
     print('Size:', list_length)
 
 
-linked_list = LinkedList()
+linked_list = SinglyLinkedList()
 
-linked_list.remove(0)
 print_all(linked_list)
 print_size(linked_list)  # 0
 
@@ -112,12 +103,16 @@ print_size(linked_list)  # 2
 
 linked_list.remove(1)
 print_all(linked_list)  # 2 3
+print_size(linked_list)  # 2
 linked_list.remove(2)
 print_all(linked_list)  # 3
+print_size(linked_list)  # 1
 linked_list.remove(3)
 print_all(linked_list)
+print_size(linked_list)  # 0
 
 linked_list.prepend(3)
 linked_list.prepend(2)
 linked_list.prepend(1)
 print_all(linked_list)  # 1 2 3
+print_size(linked_list)  # 3
