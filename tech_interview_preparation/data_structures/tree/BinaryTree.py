f723b9b83c8a1b91bfe6f49e485a80eed645da83
TK <leandrotk100@gmail.com>
2020-01-12 21:43:30 -0300

    Base objects to implement binary tree

---
+++ tech_interview_preparation/data_structures/tree/BinaryTree.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left_child = None
+        self.right_child = None
+
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
